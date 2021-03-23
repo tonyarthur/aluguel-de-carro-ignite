bdc0377b670a8b733abbbf1b95b62e5694b6a8db
Tony Arthur <tony_arthur@live.com>
2021-03-22 22:44:22 -0300

    Inserindo tipagem de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,22 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid"; 
+
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = {
-        name, 
+    const category  = new Category();
+
+    Object.assign(category, {
+        name,
         description,
-        id: uuidV4(),
-    };
+        created_at: new Date(),
+    });
 
     categories.push(category);
 
